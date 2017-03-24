42496a8cbe19a0fb7d8f2261259f96c2ceb363ff
syq <shenyongqiang2008@126.com>
2017-03-24 19:22:11 +0800

    1.屏蔽刹车逻辑 2.更改单机版玩家被其他npc撞倒的逻辑

--- bikeScripts/bike.cs
+++ bikeScripts/bike.cs
@@ -995,16 +995,26 @@ public class bike : MonoBehaviour {
 					return;
 				}
 				
-				float rand = Random.Range(0f, 100f);
+				float rand = Random.Range(0f, 10000f) % 100;
 				if(rand < 35)
 				{
 					if(!bikeScript.bIsFall && !bikeScript.bIsAiNPC)
 					{
 						bikeScript.makeBikeFall();
 					}
+					
+					if(!bIsFall && !bIsAiNPC)
+					{
+						makeBikeFall();
+					}
 				}
 				else
 				{
+					if(!bikeScript.bIsFall && bikeScript.bIsAiNPC)
+					{
+						bikeScript.makeBikeFall();
+					}
+
 					if(!bIsFall && bIsAiNPC)
 					{
 						makeBikeFall();
@@ -2542,10 +2552,10 @@ public class bike : MonoBehaviour {
 		if(!bIsAiNPC)
 		{
 			brakeState = 0;
-			if (InputEventCtrl.PlayerSC[0] > 0f) {
-				brakeState = 3;
-				timeSub *= ( (float)brakeState / 8 );
-			}
+//			if (InputEventCtrl.PlayerSC[0] > 0f) {
+//				brakeState = 3;
+//				timeSub *= ( (float)brakeState / 8 );
+//			}
 //			if(pcvr.bIsHardWare)
 //			{
 //				//get hid info
